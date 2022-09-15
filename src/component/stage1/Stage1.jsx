f2551f25e78784777e905843fec45dbeaa318029
ldulivo <ldulivo@gmail.com>
2022-09-15 16:40:01 -0300

    test-4.9 in vercel.com

--- src/component/stage1/Stage1.jsx
+++ src/component/stage1/Stage1.jsx
@@ -1,92 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
-/* import { useFruitProcessing, useNewFruitProcessing } from '../../utils/stage1' */
+import { useFruitProcessing, useNewFruitProcessing } from '../../utils/stage1'
 import { Player } from '../Player'
 import { Fruit } from './Fruit'
-
-export const useFruitProcessing = (dataFruit, screenWidth) => {
-  // eslint-disable-next-line array-callback-return
-  dataFruit.map(function (f, i) {
-    if (f.y >= 435) {
-      dataFruit[i] = f = {
-        nameFruit: useFruitName(),
-        x: useFruitRandom(screenWidth),
-        y: -30
-      }
-    } else {
-      dataFruit[i] = f = {
-        ...f,
-        y: f.y + 1
-      }
-    }
-  })
-  /* dataFruit[0] = {
-    ...dataFruit[0],
-    y: dataFruit[0].y + 1
-  }
-  if (dataFruit[0].y >= 435) {
-    dataFruit[0].y = -30
-  } */
-  /* dataFruit[0].y = 405 */
-
-  return dataFruit
-}
-
-export const useNewFruitProcessing = (dataFruit, screenWidth) => {
-  /* dataFruit[0] = {
-    nameFruit: 'apple',
-    x: useFruitRandom(screenWidth),
-    y: -30
-  } */
-  dataFruit = [
-    {
-      nameFruit: useFruitName(),
-      x: useFruitRandom(screenWidth),
-      y: -30
-    },
-    {
-      nameFruit: useFruitName(),
-      x: useFruitRandom(screenWidth),
-      y: -130
-    },
-    {
-      nameFruit: useFruitName(),
-      x: useFruitRandom(screenWidth),
-      y: -200
-    },
-    {
-      nameFruit: useFruitName(),
-      x: useFruitRandom(screenWidth),
-      y: -280
-    },
-    {
-      nameFruit: useFruitName(),
-      x: useFruitRandom(screenWidth),
-      y: -320
-    }
-  ]
-
-  return dataFruit
-}
-
-export const useFruitRandom = (screenWidth) => {
-  return (Math.floor(Math.random() * (screenWidth - 50)) + 25)
-}
-
-export const useFruitName = () => {
-  const name = Math.floor(Math.random() * 5)
-  switch (name) {
-    case 0:
-      return 'sandia'
-    case 1:
-      return 'peach'
-    case 2:
-      return 'basaha'
-    case 3:
-      return 'banana'
-    default:
-      return 'apple'
-  }
-}
 
 export const Stage1 = ({ body }) => {
   const ContainerStage1 = useRef()
@@ -105,7 +20,7 @@ export const Stage1 = ({ body }) => {
   body.classList.add('stage1')
   const movePixel = 5
 
-  const [dataFruit, setDataFruit] = useState([
+  /* const [dataFruit, setDataFruit] = useState([
     {
       nameFruit: 'sandia',
       x: 30,
@@ -131,9 +46,9 @@ export const Stage1 = ({ body }) => {
       x: 150,
       y: -320
     }
-  ])
+  ]) */
 
-  /* const [dataFruit, setDataFruit] = useState([]) */
+  const [dataFruit, setDataFruit] = useState([])
 
   const HandleClickClient = (e) => {
     setClientX({
